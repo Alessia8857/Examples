9951b16699da08fad709475fed1776a01e87cfb9
Alessia8857 <113267154+Alessia8857@users.noreply.github.com>
2022-09-13 00:48:00 +0300

    Ex003

---
+++ Ex003/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+int Prompt(string messege)
+{
+    Console.Write(messege);
+    string strValue = Console.ReadLine() ?? "0";
+    int value = int.Parse(strValue);
+    return value;
+}
+int Value = Prompt("Введите число > ");
+{
+if (Value%2 == 0)
+{
+    System.Console.WriteLine("да");
+}
+else
+{
+    System.Console.WriteLine("нет");
+}
+}
