d665ad9b67cd93d2090dc03fb525fc32c1da30d8
Alessia8857 <113267154+Alessia8857@users.noreply.github.com>
2022-09-12 22:18:56 +0300

    Ex002

--- Ex002/Program.cs
+++ Ex002/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+int Prompt(string messege)
+{
+    Console.Write(messege);
+    string strValue = Console.ReadLine() ?? "0";
+    int value = int.Parse(strValue);
+    return value;
+}
+int firstValue = Prompt("Введите первое число > ");
+int secondValue = Prompt("Введите второе число > ");
+int thirdValue = Prompt("Введите третье число > ");
+int MaxValue = firstValue;
+{
+if ( MaxValue < secondValue )
+   {
+    MaxValue = secondValue ;
+   }
+if (MaxValue < thirdValue)
+   {
+    MaxValue = thirdValue ;
+   }
+   System.Console.WriteLine ($"{MaxValue}");
+}
+ 
