b3786726c173fbc89dca5b5d5ea435066684c3a3
Alessia8857 <113267154+Alessia8857@users.noreply.github.com>
2022-09-17 13:42:40 +0300

    Lesson2

---
+++ Lesson2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int Prompt(string messege)
+{
+    Console.Write(messege);
+    string strValue = Console.ReadLine() ?? "0";
+    int value = int.Parse(strValue);
+    return value;
+}
+int a = Prompt("Введите трёхзначное число > ");
+if (a < 1000 && a > 99)
+{
+    int result = a / 10 % 10;
+    System.Console.WriteLine(result);
+}
+else
+{
+    System.Console.WriteLine(" Вы ввели не трёхзначное число ");
+}
