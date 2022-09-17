50341bbe95d0506f8e9fa911985f7bc06027de38
Alessia8857 <113267154+Alessia8857@users.noreply.github.com>
2022-09-17 17:51:05 +0300

    Lesson22

---
+++ Lesson22/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 2: Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа
+
+int value = new Random().Next(100, 1000);
+System.Console.WriteLine(value);
+int firstNumber = value / 10 / 10;
+int secondNumber = value % 10;
+{
+    System.Console.WriteLine($" {firstNumber} {secondNumber}" );
+}
+
+
