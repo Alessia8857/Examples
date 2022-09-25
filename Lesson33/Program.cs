31ccf3b63bc65423f203c3e00ab2f3eea21a91c1
Alessia8857 <113267154+Alessia8857@users.noreply.github.com>
2022-09-26 00:25:49 +0300

    Lesson33

---
+++ Lesson33/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine() ?? "0";
+    int Value = int.Parse(strValue);
+    return Value;
+}
+int Value = Prompt("Введите число > ");
+
+void CubeTable( int Value)
+{
+    int index = 1;
+    while (index <= Value)
+    {
+        System.Console.WriteLine($"Куб числа {index} это {index * index * index} ");
+        index++;
+
+    }
+}
+CubeTable(Value);
+
