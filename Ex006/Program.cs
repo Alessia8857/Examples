83ad151b6b40f954adc1fb0a8bdc63332d858ebc
Alessia <alessia8857@gmail.com>
2022-10-31 02:56:22 +0300

    Ex006

---
+++ Ex006/Program.cs
@@ -0,0 +1,20 @@
+﻿// написать программу которая из имеющегося массива строк формирует массив из строк, длина которых меньше 3
+ string [] FillArray;
+{
+    Console.WriteLine(" Введите массив строк через пробел");
+    string array = Console.ReadLine() ;
+    FillArray = array.Split(' ');
+}
+var result = new string[FillArray.Length];
+var realSize = 0;
+foreach (var value in FillArray)
+ {
+    if( value.Length < 4)
+       {
+        result[realSize]= value;
+        realSize++;
+       }
+ }
+ Console.WriteLine(string.Join(Environment.NewLine,result,0,realSize));
+Console.ReadKey(true);
+
